6b9c408b70b100dd6b43e6c9affcfb33cde4f2e2
stilrdim <51018950+stilrdim@users.noreply.github.com>
2025-07-27 13:00:36 +0100

    Performance comments for Map vs .find

--- dist/itemLoader.js
+++ dist/itemLoader.js
@@ -3,3 +3,18 @@ import itemsData from "../data/items.json" with { type: "json" };
 const itemEntries = itemsData.map((item) => [item.id, item]);
 // Create map
 export const items = new Map(itemEntries);
+/*
+6 searches:
+Map:    0.067ms
+Find:   0.049ms
+
+
+10 searches:
+Map:    0.069ms
+Find:   0.229ms
+
+
+1000 searches:
+Map:    0.088ms
+Find:   2.5ms
+*/ 
--- src/itemLoader.ts
+++ src/itemLoader.ts
@@ -6,3 +6,19 @@ const itemEntries: [string, ItemLocal][] = itemsData.map((item: ItemLocal) => [i
 
 // Create map
 export const items = new Map<string, ItemLocal>(itemEntries);
+
+/*
+6 searches: 
+Map:    0.067ms
+Find:   0.049ms
+
+
+10 searches:
+Map:    0.069ms
+Find:   0.229ms
+
+
+1000 searches:
+Map:    0.088ms
+Find:   2.5ms
+*/
