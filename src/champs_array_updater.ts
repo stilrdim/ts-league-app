b328450827c4256711b88977f97b52b3f053e058
stilrdim <stilrdim@gmail.com>
2026-06-04 21:24:08 +0100

    Update champs and add a script for future auto-update

---
+++ src/champs_array_updater.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { Agent } from "https";
+import * as cheerio from "cheerio";
+import { champNamesConfigPath } from "./config/constants.js";
+
+// Self-signed cert
+const httpsAgent = new Agent({
+  rejectUnauthorized: false,
+});
+
+const opggRequest = axios.create({
+  baseURL: `https://op.gg/lol/`,
+  httpsAgent,
+});
+
+async function loadChampPage(): Promise<string> {
+  try {
+    const { data: res } = await axios.get("https://op.gg/lol/champions");
+    return res;
+  } catch (err) {
+    console.error(err);
+    return "";
+  }
+}
+
+function getChampNames(html: string): string[] {
+  const $ = cheerio.load(html);
+
+  // Element holding all champ names
+  const champNames = $(`.w-\\[46px\\].truncate.text-xs.text-gray-500`)
+    .map((_, el) => {
+      const url = $(el).closest("a").attr("href");
+
+      if (!url) return;
+
+      const champName = url.split("champions/")[1].split("/")[0];
+      // if (champName === "build") return;
+      return champName;
+    })
+    .get();
+
+  return champNames;
+}
+
+async function main() {
+  console.log("Hi!");
+
+  const html = await loadChampPage();
+
+  const result = getChampNames(html);
+
+  result.forEach((c) => console.log(c));
+}
+
+main();
